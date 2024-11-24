11fd0e2813d24180e33af2b5bcf6f559d1300f90
SantiagoAEM <104226376+SantiagoAEM@users.noreply.github.com>
2024-11-24 15:07:00 -0600

    se cambia de localizacion db.json a root

--- api/server.js
+++ api/server.js
@@ -8,7 +8,7 @@ server.use(middlewares)
 // Add this before server.use(router)
 server.use(jsonServer.rewriter({
     '/api/*': '/$1',
-    '/products/:resource/:id/show': '/:resource/:id'
+    '/product/:resource/:id/show': '/:resource/:id'
 }))
 server.use(router)
 server.listen(3001, () => {
